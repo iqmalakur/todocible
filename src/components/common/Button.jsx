9de9b46868b7df9ead9945bb9832fb9765499f4c
iqmalakur <iakbarkurnia@gmail.com>
2024-01-09 11:14:50 +0700

    add button alignment

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -5,11 +5,23 @@ import PropTypes from "prop-types";
  * @param {string} props.children
  * @param {boolean} props.pill
  * @param {string} props.type
+ * @param {string} props.align
  * @returns {React.JSX}
  */
-function Button({ children, pill = false, type = "button" }) {
+function Button({ children, pill = false, type = "button", align = "start" }) {
+  let margin = "mr-auto";
+
+  switch (align) {
+    case "center":
+      margin = "mx-auto";
+      break;
+    case "right":
+      margin = "ms-auto";
+      break;
+  }
+
   return (
-    <button type={type} className={pill ? "btn-pill" : "btn"}>
+    <button type={type} className={`${pill ? "btn-pill" : "btn"} ${margin}`}>
       {children}
     </button>
   );
@@ -19,6 +31,7 @@ Button.propTypes = {
   children: PropTypes.string,
   pill: PropTypes.bool,
   type: PropTypes.string,
+  align: PropTypes.string,
 };
 
 export default Button;
