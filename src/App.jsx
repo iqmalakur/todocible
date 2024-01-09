ffcf8c00fbedc46fdbba1884be55376494c91857
iqmalakur <iakbarkurnia@gmail.com>
2024-01-09 09:19:10 +0700

    add react-router-dom dependency

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-center text-sky-400 text-3xl">Hello World</h1>;
-}
-
-export default App;
---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import { RouteObject } from "react-router-dom";
+
+/**
+ * @type {RouteObject[]}
+ */
+const router = [
+  {
+    path: "/",
+    element: <h1>Halaman Home</h1>,
+  },
+  {
+    path: "/task",
+    element: <h1>Halaman Task</h1>,
+  },
+];
+
+export default router;
